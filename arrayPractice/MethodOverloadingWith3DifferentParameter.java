e2a28bbaba96d39c0fadc17ac0ffd372498308b9
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-05 08:57:00 +0530

    Object & Array Practice Code

---
+++ arrayPractice/MethodOverloadingWith3DifferentParameter.java
@@ -0,0 +1,39 @@
+package com.rala.arrayPractice;
+
+public class MethodOverloadingWith3DifferentParameter {
+	
+	public static void main(String args[]) {
+		
+		Method method = new Method();
+		method.parentMethod("Sonu", 4, 'b');
+			
+		//After overloading
+		System.out.println();
+			
+		AfterOverloading afterOverloading = new AfterOverloading(); 
+		afterOverloading.afterOverloadingParentMethod("Rohit", 3, 'a');
+				
+	}
+
+	
+}
+
+class Method {
+	
+	public void parentMethod(String name, int rollNo, char grade) {
+		System.out.println("Name : " + name);
+		System.out.println("Roll No : " + rollNo);
+		System.out.println("Grade : " + grade);
+	}
+
+}
+
+class AfterOverloading extends Method {
+	
+	//Method Overloading
+	public void afterOverloadingParentMethod(String cName, int cRollNo, char cGrade) {
+		System.out.println("cName : " + cName);
+		System.out.println("cRoll No : " + cRollNo);
+		System.out.println("cGrade : " + cGrade);
+	}
+}
