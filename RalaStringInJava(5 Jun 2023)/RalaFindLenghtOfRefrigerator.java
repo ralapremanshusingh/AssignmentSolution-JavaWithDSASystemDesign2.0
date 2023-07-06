82795f6f8959cf8ab80a57fc2d4876536b11b821
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-06 23:17:20 +0530

    Find the Length of Refrigerator

---
+++ RalaStringInJava(5 Jun 2023)/RalaFindLenghtOfRefrigerator.java
@@ -0,0 +1,6 @@
+public class RalaFindLenghtOfRefrigerator {
+    public static void main(String[] args) {
+        String str="refrigerator";
+        System.out.println("Lenght of : " + str + "is :" + str.length());
+    }
+}
