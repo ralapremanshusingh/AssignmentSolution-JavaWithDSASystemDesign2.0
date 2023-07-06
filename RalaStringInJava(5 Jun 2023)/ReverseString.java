9d774fbb7fc2777998a377c45132914dbadc5ef7
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-06 09:53:00 +0530

    New Change

---
+++ RalaStringInJava(5 Jun 2023)/ReverseString.java
@@ -0,0 +1,8 @@
+public class ReverseString {
+    public static void main(String args[]) {
+    StringBuffer  stringBuffer = new StringBuffer();
+    stringBuffer.append("PWSKILLS");
+    System.out.println("Original String : " + stringBuffer);
+    System.out.println("After Reverse : " + stringBuffer.reverse());
+    }
+}
