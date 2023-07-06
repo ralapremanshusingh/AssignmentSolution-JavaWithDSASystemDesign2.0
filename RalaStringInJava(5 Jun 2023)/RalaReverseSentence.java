c69d4509160097ce99c17f619d3140b939f8b6c2
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-06 11:43:14 +0530

    gd

---
+++ RalaStringInJava(5 Jun 2023)/RalaReverseSentence.java
@@ -0,0 +1,11 @@
+public class RalaReverseSentence {
+    public static void main(String args[]) {
+        StringBuilder stringBuilder1 = new StringBuilder();
+        stringBuilder1.append("Think Twice");
+        StringBuilder stringBuilder2 = new StringBuilder();
+        stringBuilder2=stringBuilder1.substring(0, 6);
+
+
+
+    }
+}
