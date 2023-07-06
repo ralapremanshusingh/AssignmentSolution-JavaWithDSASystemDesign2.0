7f580ba1990c9af772b825227a1be52013f48b5c
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-06 23:08:01 +0530

    Compare Two String

---
+++ RalaStringInJava(5 Jun 2023)/RalaCompareTwoString.java
@@ -0,0 +1,11 @@
+public class RalaCompareTwoString {
+    public static void main(String[] args) {
+        String str1="Rohti";
+        String str2="Sonu";
+
+        System.out.println("First String : " + str1);
+        System.out.println("Second String : " + str2);
+
+        System.out.println("Both are equal : " + str1.equals(str2));
+    }
+}
