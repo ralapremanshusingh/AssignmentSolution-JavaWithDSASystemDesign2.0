1033c6ce339c4f27e0c0ac78d760d82268874f0d
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-06 22:56:27 +0530

    Concate two string

---
+++ RalaStringInJava(5 Jun 2023)/RalaConcatString.java
@@ -0,0 +1,12 @@
+public class RalaConcatString {
+    public static void main(String[] args) {
+        String str1="Rohit";
+        String str2=" Singh";
+        String str3=str1+str2;
+
+        System.out.println("First String : " + str1);
+        System.out.println("Second String : " + str2);
+        
+        System.out.println("After Concate :" + str3);
+    }
+}
---
+++ RalaStringInJava(5 Jun 2023)/RalaSrtingLength.java
@@ -0,0 +1,9 @@
+
+public class RalaSrtingLength {
+    public static void main(String[] args) {
+        String str="Rohit";
+        System.out.println("Your Name : "+str);
+        System.out.println("Length : " + str.length());
+    }
+
+}
