3bf988d772f6b417d50480f55a87d52611b68e73
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-07-06 20:17:39 +0530

    ReverseSentence

--- RalaStringInJava(5 Jun 2023)/RalaReverseSentence.java
+++ RalaStringInJava(5 Jun 2023)/RalaReverseSentence.java
@@ -1,11 +1,15 @@
 public class RalaReverseSentence {
     public static void main(String args[]) {
-        StringBuilder stringBuilder1 = new StringBuilder();
-        stringBuilder1.append("Think Twice");
-        StringBuilder stringBuilder2 = new StringBuilder();
-        stringBuilder2=stringBuilder1.substring(0, 6);
-
-
+        String str= "Think Twice";
+        String[] arrstr = str.split(" ");
+        String revstr="";
+        
+        for(String vstr: arrstr) {
+            for(int i=vstr.length()-1; i>=0; i--)
+                revstr+=vstr.charAt(i);
+            revstr=revstr+" ";
+        }
+        System.out.println(revstr);
 
     }
 }
